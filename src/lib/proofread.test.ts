7bddf29167cb7d8171ca5044d9e263dc7b108ca5
Tim Wong <cytimo@gmail.com>
2020-12-10 18:16:12 +0800

    add the test cases for lib/proofread

---
+++ src/lib/proofread.test.ts
@@ -0,0 +1,373 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { SinonStub, stub } from 'sinon';
+import * as Convert from './convert';
+import { proofread } from './proofread';
+import { TemplateParseOutput } from './types';
+
+describe('lib/proofread', () => {
+  let convertStub: SinonStub, testTmplParsed: TemplateParseOutput;
+
+  beforeAll(() => {
+    convertStub = stub(Convert, 'convert');
+    testTmplParsed = JSON.parse(readFileSync(resolve(__dirname, '../__test__/data/template.parsed.json')).toString());
+  });
+
+  afterAll(() => {
+    convertStub.restore();
+  });
+
+  beforeEach(() => {
+    convertStub.reset();
+    convertStub.returns('[converted]');
+  });
+
+  it('parse all the variables correctly', () => {
+    const testDotenvParsed = {
+      REQUIRED__WITHOUT_NAME: '--- unchanged --',
+      REQUIRED__WITH_NAME: '--- unchanged --',
+      REQUIRED__MULTI_TYPE: '--- unchanged --',
+      OPTIONAL__WITHOUT_NAME: '--- unchanged --',
+      OPTIONAL__WITHOUT_NAME_BUT_DEFAULT: '--- unchanged --',
+      OPTIONAL__WITH_NAME: '--- unchanged --',
+      OPTIONAL__WITH_NAME_AND_DEFAULT: '--- unchanged --',
+      OPTIONAL__MULTI_TYPE: '--- unchanged --',
+      OPTIONAL__STRING_ARRAY: '--- unchanged --',
+      OPTIONAL__INTEGER: '--- unchanged --',
+      OPTIONAL__FLOAT: '--- unchanged --',
+      OPTIONAL__NUMBER_ARRAY: '--- unchanged --',
+      OPTIONAL__TRUE: '--- unchanged --',
+      OPTIONAL__FALSE: '--- unchanged --',
+      OPTIONAL__BOOLEAN_ARRAY: '--- unchanged --',
+      OPTIONAL__JSON_ARRAY: '--- unchanged --',
+      OPTIONAL__JSON_OBJECT: '--- unchanged --',
+      MISSING_ANNOTATION: '--- unchanged --',
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+    const expectedRawEnv = {
+      REQUIRED__WITHOUT_NAME: '[converted]',
+      REQUIRED__WITH_NAME: '[converted]',
+      REQUIRED__MULTI_TYPE: '[converted]',
+      OPTIONAL__WITHOUT_NAME: '[converted]',
+      OPTIONAL__WITHOUT_NAME_BUT_DEFAULT: '[converted]',
+      OPTIONAL__WITH_NAME: '[converted]',
+      OPTIONAL__WITH_NAME_AND_DEFAULT: '[converted]',
+      OPTIONAL__MULTI_TYPE: '[converted]',
+      OPTIONAL__STRING_ARRAY: '[converted]',
+      OPTIONAL__INTEGER: '[converted]',
+      OPTIONAL__FLOAT: '[converted]',
+      OPTIONAL__NUMBER_ARRAY: '[converted]',
+      OPTIONAL__TRUE: '[converted]',
+      OPTIONAL__FALSE: '[converted]',
+      OPTIONAL__BOOLEAN_ARRAY: '[converted]',
+      OPTIONAL__JSON_ARRAY: '[converted]',
+      OPTIONAL__JSON_OBJECT: '[converted]',
+      MISSING_ANNOTATION: '--- unchanged --',
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+
+    const { error, rawEnv, env } = proofread(testDotenvParsed, testTmplParsed);
+    expect(error).toBeUndefined();
+    expect(rawEnv).toStrictEqual(expectedRawEnv);
+    expect(env).toStrictEqual(expectedRawEnv);
+  });
+
+  it('fill up all optional variables with default values', () => {
+    const testDotenvParsed = {
+      REQUIRED__WITHOUT_NAME: '--- unchanged --',
+      REQUIRED__WITH_NAME: '--- unchanged --',
+      REQUIRED__MULTI_TYPE: '--- unchanged --',
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+    const expectedRawEnv = {
+      REQUIRED__WITHOUT_NAME: '[converted]',
+      REQUIRED__WITH_NAME: '[converted]',
+      REQUIRED__MULTI_TYPE: '[converted]',
+      OPTIONAL__WITHOUT_NAME: null,
+      OPTIONAL__WITHOUT_NAME_BUT_DEFAULT: 'foo',
+      OPTIONAL__WITH_NAME: null,
+      OPTIONAL__WITH_NAME_AND_DEFAULT: 'foobar',
+      OPTIONAL__MULTI_TYPE: null,
+      OPTIONAL__STRING_ARRAY: ['xxx', 'yyy', 'zzz'],
+      OPTIONAL__INTEGER: 777,
+      OPTIONAL__FLOAT: 3.14,
+      OPTIONAL__NUMBER_ARRAY: [777, 3.14],
+      OPTIONAL__TRUE: true,
+      OPTIONAL__FALSE: false,
+      OPTIONAL__BOOLEAN_ARRAY: [true, true, false, false],
+      OPTIONAL__JSON_ARRAY: ['xxx', 'yyy', 'zzz'],
+      OPTIONAL__JSON_OBJECT: { foo: 'bar' },
+      MISSING_ANNOTATION: null,
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+
+    const { error, rawEnv, env } = proofread(testDotenvParsed, testTmplParsed);
+    expect(error).toBeUndefined();
+    expect(rawEnv).toStrictEqual(expectedRawEnv);
+    expect(env).toStrictEqual(expectedRawEnv);
+  });
+
+  it('remove the unknown variable', () => {
+    const testDotenvParsed = {
+      REQUIRED__WITHOUT_NAME: '--- unchanged --',
+      REQUIRED__WITH_NAME: '--- unchanged --',
+      REQUIRED__MULTI_TYPE: '--- unchanged --',
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+    const expectedRawEnv = {
+      REQUIRED__WITHOUT_NAME: '[converted]',
+      REQUIRED__WITH_NAME: '[converted]',
+      REQUIRED__MULTI_TYPE: '[converted]',
+      OPTIONAL__WITHOUT_NAME: null,
+      OPTIONAL__WITHOUT_NAME_BUT_DEFAULT: 'foo',
+      OPTIONAL__WITH_NAME: null,
+      OPTIONAL__WITH_NAME_AND_DEFAULT: 'foobar',
+      OPTIONAL__MULTI_TYPE: null,
+      OPTIONAL__STRING_ARRAY: ['xxx', 'yyy', 'zzz'],
+      OPTIONAL__INTEGER: 777,
+      OPTIONAL__FLOAT: 3.14,
+      OPTIONAL__NUMBER_ARRAY: [777, 3.14],
+      OPTIONAL__TRUE: true,
+      OPTIONAL__FALSE: false,
+      OPTIONAL__BOOLEAN_ARRAY: [true, true, false, false],
+      OPTIONAL__JSON_ARRAY: ['xxx', 'yyy', 'zzz'],
+      OPTIONAL__JSON_OBJECT: { foo: 'bar' },
+      MISSING_ANNOTATION: null,
+      // [should be removed] UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+
+    const { error, rawEnv, env } = proofread(testDotenvParsed, testTmplParsed, { removeUnknownVariables: true });
+    expect(error).toBeUndefined();
+    expect(rawEnv).toStrictEqual(expectedRawEnv);
+    expect(env).toStrictEqual(expectedRawEnv);
+  });
+
+  it('rename the variables, using default rename options', () => {
+    const testDotenvParsed = {
+      REQUIRED__WITHOUT_NAME: '--- unchanged --',
+      REQUIRED__WITH_NAME: '--- unchanged --',
+      REQUIRED__MULTI_TYPE: '--- unchanged --',
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+    const expectedRawEnv = {
+      REQUIRED__WITHOUT_NAME: '[converted]',
+      REQUIRED__WITH_NAME: '[converted]',
+      REQUIRED__MULTI_TYPE: '[converted]',
+      OPTIONAL__WITHOUT_NAME: null,
+      OPTIONAL__WITHOUT_NAME_BUT_DEFAULT: 'foo',
+      OPTIONAL__WITH_NAME: null,
+      OPTIONAL__WITH_NAME_AND_DEFAULT: 'foobar',
+      OPTIONAL__MULTI_TYPE: null,
+      OPTIONAL__STRING_ARRAY: ['xxx', 'yyy', 'zzz'],
+      OPTIONAL__INTEGER: 777,
+      OPTIONAL__FLOAT: 3.14,
+      OPTIONAL__NUMBER_ARRAY: [777, 3.14],
+      OPTIONAL__TRUE: true,
+      OPTIONAL__FALSE: false,
+      OPTIONAL__BOOLEAN_ARRAY: [true, true, false, false],
+      OPTIONAL__JSON_ARRAY: ['xxx', 'yyy', 'zzz'],
+      OPTIONAL__JSON_OBJECT: { foo: 'bar' },
+      MISSING_ANNOTATION: null,
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+    const expectedEnv = {
+      required: {
+        withoutName: '[converted]',
+        multiType: '[converted]',
+      },
+      requiredWithName: '[converted]',
+      optional: {
+        withoutName: null,
+        withoutNameButDefault: 'foo',
+        multiType: null,
+        stringArray: ['xxx', 'yyy', 'zzz'],
+        integer: 777,
+        float: 3.14,
+        numberArray: [777, 3.14],
+        true: true,
+        false: false,
+        booleanArray: [true, true, false, false],
+        jsonArray: ['xxx', 'yyy', 'zzz'],
+        jsonObject: { foo: 'bar' },
+      },
+      optionalWithName: null,
+      optionalWithNameAndDefault: 'foobar',
+      missingAnnotation: null,
+      unknownVariable: '--- unchanged --',
+    };
+
+    const { error, rawEnv, env } = proofread(testDotenvParsed, testTmplParsed, { rename: { enabled: true } });
+    expect(error).toBeUndefined();
+    expect(rawEnv).toStrictEqual(expectedRawEnv);
+    expect(env).toStrictEqual(expectedEnv);
+  });
+
+  it('rename the variables, with caseStyle = "snake_case"', () => {
+    const testDotenvParsed = {
+      REQUIRED__WITHOUT_NAME: '--- unchanged --',
+      REQUIRED__WITH_NAME: '--- unchanged --',
+      REQUIRED__MULTI_TYPE: '--- unchanged --',
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+    const expectedRawEnv = {
+      REQUIRED__WITHOUT_NAME: '[converted]',
+      REQUIRED__WITH_NAME: '[converted]',
+      REQUIRED__MULTI_TYPE: '[converted]',
+      OPTIONAL__WITHOUT_NAME: null,
+      OPTIONAL__WITHOUT_NAME_BUT_DEFAULT: 'foo',
+      OPTIONAL__WITH_NAME: null,
+      OPTIONAL__WITH_NAME_AND_DEFAULT: 'foobar',
+      OPTIONAL__MULTI_TYPE: null,
+      OPTIONAL__STRING_ARRAY: ['xxx', 'yyy', 'zzz'],
+      OPTIONAL__INTEGER: 777,
+      OPTIONAL__FLOAT: 3.14,
+      OPTIONAL__NUMBER_ARRAY: [777, 3.14],
+      OPTIONAL__TRUE: true,
+      OPTIONAL__FALSE: false,
+      OPTIONAL__BOOLEAN_ARRAY: [true, true, false, false],
+      OPTIONAL__JSON_ARRAY: ['xxx', 'yyy', 'zzz'],
+      OPTIONAL__JSON_OBJECT: { foo: 'bar' },
+      MISSING_ANNOTATION: null,
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+    const expectedEnv = {
+      required: {
+        without_name: '[converted]',
+        multi_type: '[converted]',
+      },
+      requiredWithName: '[converted]',
+      optional: {
+        without_name: null,
+        without_name_but_default: 'foo',
+        multi_type: null,
+        string_array: ['xxx', 'yyy', 'zzz'],
+        integer: 777,
+        float: 3.14,
+        number_array: [777, 3.14],
+        true: true,
+        false: false,
+        boolean_array: [true, true, false, false],
+        json_array: ['xxx', 'yyy', 'zzz'],
+        json_object: { foo: 'bar' },
+      },
+      optionalWithName: null,
+      optionalWithNameAndDefault: 'foobar',
+      missing_annotation: null,
+      unknown_variable: '--- unchanged --',
+    };
+
+    const { error, rawEnv, env } = proofread(testDotenvParsed, testTmplParsed, { rename: { caseStyle: 'snake_case' } });
+    expect(error).toBeUndefined();
+    expect(rawEnv).toStrictEqual(expectedRawEnv);
+    expect(env).toStrictEqual(expectedEnv);
+  });
+
+  it('rename the variables, with nestingDelimiter = "_"', () => {
+    const testDotenvParsed = {
+      REQUIRED__WITHOUT_NAME: '--- unchanged --',
+      REQUIRED__WITH_NAME: '--- unchanged --',
+      REQUIRED__MULTI_TYPE: '--- unchanged --',
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+    const expectedRawEnv = {
+      REQUIRED__WITHOUT_NAME: '[converted]',
+      REQUIRED__WITH_NAME: '[converted]',
+      REQUIRED__MULTI_TYPE: '[converted]',
+      OPTIONAL__WITHOUT_NAME: null,
+      OPTIONAL__WITHOUT_NAME_BUT_DEFAULT: 'foo',
+      OPTIONAL__WITH_NAME: null,
+      OPTIONAL__WITH_NAME_AND_DEFAULT: 'foobar',
+      OPTIONAL__MULTI_TYPE: null,
+      OPTIONAL__STRING_ARRAY: ['xxx', 'yyy', 'zzz'],
+      OPTIONAL__INTEGER: 777,
+      OPTIONAL__FLOAT: 3.14,
+      OPTIONAL__NUMBER_ARRAY: [777, 3.14],
+      OPTIONAL__TRUE: true,
+      OPTIONAL__FALSE: false,
+      OPTIONAL__BOOLEAN_ARRAY: [true, true, false, false],
+      OPTIONAL__JSON_ARRAY: ['xxx', 'yyy', 'zzz'],
+      OPTIONAL__JSON_OBJECT: { foo: 'bar' },
+      MISSING_ANNOTATION: null,
+      UNKNOWN_VARIABLE: '--- unchanged --',
+    };
+    const expectedEnv = {
+      required: {
+        without: {
+          name: '[converted]',
+        },
+        multi: {
+          type: '[converted]',
+        },
+      },
+      requiredWithName: '[converted]',
+      optional: {
+        without: {
+          name: {
+            but: {
+              default: 'foo',
+            },
+          },
+        },
+        multi: {
+          type: null,
+        },
+        string: {
+          array: ['xxx', 'yyy', 'zzz'],
+        },
+        integer: 777,
+        float: 3.14,
+        number: {
+          array: [777, 3.14],
+        },
+        true: true,
+        false: false,
+        boolean: {
+          array: [true, true, false, false],
+        },
+        json: {
+          array: ['xxx', 'yyy', 'zzz'],
+          object: {
+            foo: 'bar',
+          },
+        },
+      },
+      optionalWithName: null,
+      optionalWithNameAndDefault: 'foobar',
+      missing: {
+        annotation: null,
+      },
+      unknown: {
+        variable: '--- unchanged --',
+      },
+    };
+
+    const { error, rawEnv, env } = proofread(testDotenvParsed, testTmplParsed, { rename: { nestingDelimiter: '_' } });
+    expect(error).toBeUndefined();
+    expect(rawEnv).toStrictEqual(expectedRawEnv);
+    expect(env).toStrictEqual(expectedEnv);
+  });
+
+  it('throw error because a required variable is missing', () => {
+    const testDotenvParsed = {
+      REQUIRED__WITHOUT_NAME: '-- unchanged --',
+      REQUIRED__WITH_NAME: '-- unchanged --',
+    };
+
+    const { error } = proofread(testDotenvParsed, testTmplParsed);
+    expect(error?.message).toContain('Required variable [REQUIRED__MULTI_TYPE] is missing');
+  });
+
+  it('bubble up the ConvertError when a variable value cannot be converted to the specified data type(s)', () => {
+    convertStub.throws(new Convert.ConvertError('Mock conversion error'));
+
+    const testDotenvParsed = {
+      REQUIRED__WITHOUT_NAME: '-- unchanged --',
+      REQUIRED__WITH_NAME: '-- unchanged --',
+      REQUIRED__MULTI_TYPE: '-- unchanged --',
+      OPTIONAL__INTEGER: '-- unchanged --',
+    };
+
+    const { error } = proofread(testDotenvParsed, testTmplParsed);
+    expect(error).toBeInstanceOf(Convert.ConvertError);
+  });
+});
